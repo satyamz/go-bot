6b010fd77c1aabd4694e09769a527dacb07169a6
Satyam Zode <satyam.zode@cloudbyte.com>
2017-12-17 23:30:50 +0530

    Use Go nlopes slack library

--- main.go
+++ main.go
@@ -4,40 +4,20 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/go-chat-bot/bot"
-	"github.com/go-chat-bot/bot/slack"
-	_ "github.com/go-chat-bot/plugins/catgif"
-	// _ "github.com/go-chat-bot/plugins/chucknorris"
-	// Import all the commands you wish to use
+	"github.com/nlopes/slack"
 )
 
 func main() {
-	slack.Run(os.Getenv("SLACK_TOKEN"))
-}
-
-func hello(command *bot.Cmd) (msg string, err error) {
-	msg = fmt.Sprintf("Hello %s", command.User.RealName)
-	return
-}
-
-func greet(command *bot.Cmd) (bot.CmdResult, error) {
-	commandResult := &bot.CmdResult{}
-	commandResult.Channel = "general"
-	commandResult.Message = "Hey! Go bot here.."
-	return *commandResult, nil
-}
-
-func init() {
-
-	bot.RegisterCommandV2(
-		"greet",
-		"Sends a greeting to channel ",
-		"sub command",
-		greet)
-
-	bot.RegisterCommand(
-		"hello",
-		"Sends a 'Hello' message to you on the channel.",
-		"",
-		hello)
+	api := slack.New(os.Getenv("SLACK_TOKEN"))
+	// If you set debugging, it will log all requests to the console
+	// Useful when encountering issues
+	api.SetDebug(true)
+	groups, err := api.GetGroups(false)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+		return
+	}
+	for _, group := range groups {
+		fmt.Printf("ID: %s, Name: %s\n", group.ID, group.Name)
+	}
 }
